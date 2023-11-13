7bfbcdf6cf6fc51791a7d8bfbd85f1b041312d15
Алекс Южный <youzhny.foto@gmail.com>
2023-11-13 15:22:13 +0200

    init project

---
+++ components/footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <footer className='bg-white border-t'>
+            <div className='mx-auto py-10'>
+                <p className='text-center text-xs text-black'>
+                    &copy; 2023{' '}
+                    <a
+                        className='underline'
+                        href='https://github.com/AlexPirko'>
+                        Pirko prod.
+                    </a>{' '}
+                    All rights reserved
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
