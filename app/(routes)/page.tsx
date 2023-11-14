cfde7087a5cf38b112a170854f640ef697a87f0a
Алекс Южный <youzhny.foto@gmail.com>
2023-11-14 15:05:45 +0200

    add products to page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,24 @@
+import getBillboard from '@/actions/get-billboard';
+import getProducts from '@/actions/get-products';
+import Billboard from '@/components/billboard';
+import ProductList from '@/components/product-list';
+import Container from '@/components/ui/container';
 
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
+    const billboard = await getBillboard(
+        'd27dfc8a-8c52-43cb-9ac3-ccd92edac70f',
+    );
+    return (
+        <Container>
+            <div className='space-y-10 pb-10'>
+                <Billboard data={billboard} />
+                <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+                    <ProductList title='Featured Products' items={products} />
+                </div>
+            </div>
+        </Container>
+    );
+};
 
-const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
-
-export default HomePage
+export default HomePage;
