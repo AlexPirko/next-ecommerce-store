58fcb6b3230ebb297d3e366cd96f03d4682033ba
Алекс Южный <youzhny.foto@gmail.com>
2023-11-14 19:57:30 +0200

    add modal to card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { MouseEventHandler } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Expand, ShoppingCart } from 'lucide-react';
@@ -6,6 +7,7 @@ import { Expand, ShoppingCart } from 'lucide-react';
 import { Product } from '@/types';
 import { IconButton } from './icon-button';
 import Currency from './currency';
+import usePreviewModal from '@/hooks/use-preview-modal';
 
 interface ProductCardProps {
     data: Product;
@@ -13,9 +15,16 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     const router = useRouter();
+    const previewModal = usePreviewModal();
 
     const handleClick = () => {
         router.push(`/product/${data?.id}`);
+    };
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        previewModal.onOpen(data);
     };
     return (
         <div
@@ -31,7 +40,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
                 <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
                     <div className='flex gap-x-6 justify-center'>
                         <IconButton
-                            onClick={() => {}}
+                            onClick={onPreview}
                             icon={
                                 <Expand size={20} className='text-gray-600' />
                             }
